68a948777e1f4e1be6cd02af63ff5757f16ae380
Jacob Templeton <jacob.templeton@gmail.com>
2023-04-19 17:33:02 -0700

    Add new pg pool connection, add endpoint and GET request to return all notes in the db

---
+++ controller.js
@@ -0,0 +1,23 @@
+const { pool } = require("./config");
+
+const getNotes = async (req, res, next) => {
+  try {
+    const data = await pool.query("SELECT * from notes");
+
+    if (data.rowCount == 0) {
+      return res.status(404).send("No note exists");
+    }
+
+    return res.status(200).json({
+      status: 200,
+      message: "All notes",
+      data: data.rows,
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+module.exports = {
+  getNotes,
+};
