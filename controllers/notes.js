41a0c2282410bc1209535adf4e5e482fe14f6180
Jacob Templeton <jacob.templeton@gmail.com>
2023-04-20 15:08:00 -0700

    Add ability to GET a specific note based on its id

--- controllers/notes.js
+++ controllers/notes.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 
 router.get("/", async (req, res, next) => {
   try {
-    const data = await pool.query("SELECT * from notes");
+    const data = await pool.query("SELECT * FROM notes");
 
     if (data.rowCount == 0) {
       return res.status(404).send("No note exists");
@@ -12,6 +12,28 @@ router.get("/", async (req, res, next) => {
     return res.status(200).json({
       status: 200,
       message: "All notes",
+      data: data.rows,
+    });
+  } catch (error) {
+    return next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  const id = parseInt(req.params.id);
+  const query = "SELECT * FROM notes WHERE id=$1";
+  const value = [id];
+
+  try {
+    const data = await pool.query(query, value);
+
+    if (data.rowCount == 0) {
+      return res.status(404).send("No note exists");
+    }
+
+    return res.status(200).json({
+      status: 200,
+      message: "Note",
       data: data.rows,
     });
   } catch (error) {
